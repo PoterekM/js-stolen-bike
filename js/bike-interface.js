95de603a259ed971c3ba13e086f53900ef1c5917
Brittany Kerr and Michelle Poterek <brittany@>
2017-08-03 15:38:16 -0700

    change stoleness = proximity in api call

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -2,6 +2,7 @@ var Bikes = require('./../js/bikes.js').bikesModule;
 
 var displayBikes = function(manufacturer, location, bikeData) {
   bikeData.forEach(function(bike){
+    console.log(bike);
     $('#results').append("<li>" + bike.stolen_location + " " + bike.title + " " + "serial number " + bike.serial + " " + bike.frame_colors + "</li>");
   });
 };
--- js/bikes.js
+++ js/bikes.js
@@ -6,11 +6,11 @@ function Bikes() {
 
 
 Bikes.prototype.getBikes = function(manufacturer, location, displayBikes) {
-    $.get('https://bikeindex.org/api/v3/search?manufacturer=' + manufacturer + '&location=' + location + '&distance=10&stolenness=stolen')
+    $.get('https://bikeindex.org/api/v3/search?manufacturer=' + manufacturer + '&location=' + location + '&distance=10&stolenness=proximity')
     .then(function(response) {
         displayBikes(manufacturer, location, response.bikes);
-        console.log(location);
     }).fail(function(error) {
+      console.log(location)
       $('#results').text(error.responseJSON.message);
     });
 };
