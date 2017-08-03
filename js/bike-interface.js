6bfc1271a47ec21c9a92468737f1bf6cba01d721
Brittany Kerr and Michelle Poterek <brittany@>
2017-08-03 09:22:41 -0700

    added content to user interface

---
+++ js/bike-interface.js
@@ -0,0 +1,16 @@
+var recoveredBikes = require('./../js/bikes.js').recoveredBikesModule;
+
+var displayRecoveredBikes = function(city, bikeData) {
+  $('.results').append("<li>" + bikeData + "</li>");
+}
+
+
+
+$(document).ready(function(){
+  $('#bike-location-form').submit(function(event){
+    event.preventDefault();
+    var city = $('#city').val();
+    $('#city').val("");
+    recoveredBikeObject.getBikes(city, displayRecoveredBikes)
+  });
+});
